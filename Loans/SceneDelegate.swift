ecc5afc7821f33c2e4ae2d64dffd89ef47919044
Ravil <star.toni@mail.ru>
2023-12-15 19:18:28 +0600

    [LO] SceneDelegate

--- Loans/SceneDelegate.swift
+++ Loans/SceneDelegate.swift
@@ -14,9 +14,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.overrideUserInterfaceStyle = .light
-        window.rootViewController =
-        UINavigationController(rootViewController: TabBarViewController())
+
+        let isUserLoggedIn = UserDefaults.standard.string(forKey: "userEmail") != nil && UserDefaults.standard.string(forKey: "userPassword") != nil
+
+        window.rootViewController = isUserLoggedIn ?
+            UINavigationController(rootViewController: TabBarViewController()) :
+            UINavigationController(rootViewController: OnboardingViewController())
+
         window.makeKeyAndVisible()
         self.window = window
     }
