0372a03a58df02d68a67be4ca7a74621a3ce5a27
Ravil <star.toni@mail.ru>
2023-12-15 09:38:42 +0600

    [LO] TabBarViewController

---
+++ Loans/TabBarViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TabBarViewController.swift
+//  Quotex Option
+//
+//  Created by Ravil on 10.12.2023.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addedTabBarSetup()
+    }
+        
+    private func addedTabBarSetup() {
+        let tab1 = UINavigationController(rootViewController: AmountViewController())
+        let tab2 = UINavigationController(rootViewController: OnboardingViewController())
+        let tab3 = UINavigationController(rootViewController: RequestSuccessViewController())
+        let tab4 = UINavigationController(rootViewController: RegistrationViewController())
+        let tab5 = UINavigationController(rootViewController: LoginViewController())
+        
+        tabBar.layer.borderWidth = 0.5
+        tabBar.layer.borderColor = UIColor.lightGray.cgColor
+        
+        tab1.tabBarItem = UITabBarItem(
+            title: "", image: UIImage(named: "tab1")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "tab1Active")?.withRenderingMode(.alwaysOriginal)
+        )
+
+        tab2.tabBarItem = UITabBarItem(
+            title: "", image: UIImage(named: "tab2")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "tab2Active")?.withRenderingMode(.alwaysOriginal)
+        )
+        
+        tab3.tabBarItem = UITabBarItem(
+            title: "", image: UIImage(named: "tab3")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "tab3Active")?.withRenderingMode(.alwaysOriginal)
+        )
+        
+        tab4.tabBarItem = UITabBarItem(
+            title: "", image: UIImage(named: "tab4")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "tab4Active")?.withRenderingMode(.alwaysOriginal)
+        )
+        
+        tab5.tabBarItem = UITabBarItem(
+            title: "", image: UIImage(named: "tab5")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "tab5Active")?.withRenderingMode(.alwaysOriginal)
+        )
+        
+        viewControllers = [tab1, tab2, tab3, tab4, tab5]
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
+
